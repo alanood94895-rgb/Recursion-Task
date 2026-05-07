c9b7d08afb383495654fca7304a5fd671a17a44c
CompanyProfile <alanood94895@gmail.com>
2026-05-07 13:27:58 +0400

    SumDigits

--- src/SumDigits.java
+++ src/SumDigits.java
@@ -1,2 +1,12 @@
 public class SumDigits {
-}
+
+        int sumDigits(int n) {
+
+            if (n == 0) {
+                return 0;
+            }
+
+            return (n % 10) + sumDigits(n / 10);
+        }
+    }
+
