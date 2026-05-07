055840ae3238b3eadb92fe2a2cffb345f845b142
CompanyProfile <alanood94895@gmail.com>
2026-05-07 09:09:25 +0400

    create BunnyEarsDemo class

--- src/BunnyEarsDemo.java
+++ src/BunnyEarsDemo.java
@@ -4,7 +4,7 @@ public class BunnyEarsDemo {
 
         BunnyEars bunnyEars = new BunnyEars();
 
-        System.out.println();
+        System.out.println("bunnyEars2(0) " + bunnyEars.bunnyEars2(0));
         System.out.println();
         System.out.println();
 
