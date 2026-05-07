dbed753915da6e01240e6fee171fa7ad07fc4fe5
CompanyProfile <alanood94895@gmail.com>
2026-05-07 11:37:24 +0400

    NoX

--- src/NoX.java
+++ src/NoX.java
@@ -1,2 +1,45 @@
 public class NoX {
+    // Class that contains the recursive noX method
+    class NoX {
+
+        // Method to remove all 'x' characters from a string
+        String noX(String str) {
+
+            // Base case: if string is empty
+            if (str.length() == 0) {
+                return "";
+            }
+
+            // Get the first character
+            char firstChar = str.charAt(0);
+
+            // Process the remaining string
+            String remaining = noX(str.substring(1));
+
+            // If first character is 'x', skip it
+            if (firstChar == 'x') {
+                return remaining;
+            }
+
+            // Otherwise include the character
+            return firstChar + remaining;
+        }
+    }
+
+    // Demo class with the main method
+    public class NoXDemo {
+
+        public static void main(String[] args) {
+
+            // Create object of NoX class
+            NoX remover = new NoX();
+
+            // Test the noX method with different strings
+            System.out.println("noX(\"xaxb\") → \"" + remover.noX("xaxb") + "\"");
+
+            System.out.println("noX(\"abc\") → \"" + remover.noX("abc") + "\"");
+
+            System.out.println("noX(\"xx\") → \"" + remover.noX("xx") + "\"");
+        }
+    }
 }
