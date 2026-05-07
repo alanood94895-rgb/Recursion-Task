cae4c37732df27ca86d5e5a6f3c0f88670a2a6bb
CompanyProfile <alanood94895@gmail.com>
2026-05-07 11:25:22 +0400

    Count8Demo

--- src/Count8Demo.java
+++ src/Count8Demo.java
@@ -2,7 +2,7 @@ public class Count8Demo {
         public static void main(String[] args) {
             Count8 counter = new Count8();
 
-            System.out.println("count8(8) → " + counter.count8(8));
+            System.out.println( counter.count8(8));
 
             System.out.println( counter.count8(818));
 
