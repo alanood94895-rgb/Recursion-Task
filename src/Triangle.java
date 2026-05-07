f4bdc15181a01de4af0943bf33b72436945af737
CompanyProfile <alanood94895@gmail.com>
2026-05-07 11:13:40 +0400

    Triangle

--- src/Triangle.java
+++ src/Triangle.java
@@ -26,4 +26,3 @@ public class Triangle {
             // Test the triangle method with different values
             System.out.println("triangle(0) → " + tri.triangle(0));
 
-            System.out.println("triangle(1) → " + tri.triangle(1));
