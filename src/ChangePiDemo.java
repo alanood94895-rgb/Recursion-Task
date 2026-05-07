0bb21c0a4a5d615cc6c952be16b98ea96a2c5962
CompanyProfile <alanood94895@gmail.com>
2026-05-07 09:54:41 +0400

    ChangePiDemo

--- src/ChangePiDemo.java
+++ src/ChangePiDemo.java
@@ -1,7 +1,6 @@
 public class ChangePiDemo {
     public static void main(String[] args) {
 
-        // Create an object of the ChangePi class
         ChangePi changer = new ChangePi();
 
         System.out.println("changePi(xpix)" + changer.changePi("xpix"));
