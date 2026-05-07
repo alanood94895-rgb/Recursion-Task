2daae9ac309aeb0c31237313862e5cabf2dfeba3
CompanyProfile <alanood94895@gmail.com>
2026-05-07 11:13:35 +0400

    Triangle

--- src/Triangle.java
+++ src/Triangle.java
@@ -27,5 +27,3 @@ public class Triangle {
             System.out.println("triangle(0) → " + tri.triangle(0));
 
             System.out.println("triangle(1) → " + tri.triangle(1));
-
-            System.out.println("triangle(2) → " + tri.triangle(2));
