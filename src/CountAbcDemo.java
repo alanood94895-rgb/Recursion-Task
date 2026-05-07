4bd53b96bcdf18d6aaa9c75a2b38b2d15cfac90b
CompanyProfile <alanood94895@gmail.com>
2026-05-07 10:17:40 +0400

    CountAbcDemo

--- src/CountAbcDemo.java
+++ src/CountAbcDemo.java
@@ -5,10 +5,7 @@ public class CountAbcDemo {
         // Method to count occurrences of "abc" and "aba"
         int countAbc(String str) {
 
-            // Base case: if string length is less than 3
-            if (str.length() < 3) {
-                return 0;
-            }
+
 
 
 
