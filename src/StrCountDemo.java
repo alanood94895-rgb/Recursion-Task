d3b7c7302d0dedd7214c9ae8912674ad6d930a47
CompanyProfile <alanood94895@gmail.com>
2026-05-07 10:28:00 +0400

    StrCountDemo

--- src/StrCountDemo.java
+++ src/StrCountDemo.java
@@ -5,10 +5,6 @@ public class StrCountDemo {
         // Method to count non-overlapping occurrences of a substring
         int strCount(String str, String sub) {
 
-            // Base case: if str is shorter than sub
-            if (str.length() < sub.length()) {
-                return 0;
-            }
 
 
 
