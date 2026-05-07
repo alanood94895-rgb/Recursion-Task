32a509db858386bb58e08702b6f6645466f9be9b
CompanyProfile <alanood94895@gmail.com>
2026-05-07 09:40:54 +0400

    CountXDemo

--- src/CountXDemo.java
+++ src/CountXDemo.java
@@ -1,2 +1,12 @@
 public class CountXDemo {
+    public static void main(String[] args) {
+
+        // Create an object of the CountX class
+        CountX counter = new CountX();
+
+        // Test the countX method with different strings
+        System.out.println("countX(\"xxhixx\") → " + counter.countX("xxhixx"));
+        System.out.println("countX(\"xhixhix\") → " + counter.countX("xhixhix"));
+        System.out.println("countX(\"hi\") → " + counter.countX("hi"));
+    }
 }
