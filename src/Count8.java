685efc49e7ea1b65746c9148d7ff2542d9a483fe
CompanyProfile <alanood94895@gmail.com>
2026-05-07 11:19:38 +0400

    Count8

--- src/Count8.java
+++ src/Count8.java
@@ -39,15 +39,3 @@ public class Count8 {
 
         public static void main(String[] args) {
 
-            // Create an object of Count8 class
-            Count8 counter = new Count8();
-
-            // Test the count8 method with different values
-            System.out.println("count8(8) → " + counter.count8(8));
-
-            System.out.println("count8(818) → " + counter.count8(818));
-
-            System.out.println("count8(8818) → " + counter.count8(8818));
-        }
-    }
-}
