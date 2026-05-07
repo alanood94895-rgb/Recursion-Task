dbaa45c7df208b69b458ca35779b7a032aad63fa
CompanyProfile <alanood94895@gmail.com>
2026-05-07 12:14:24 +0400

    Count11

--- src/Count11.java
+++ src/Count11.java
@@ -15,20 +15,3 @@ public class Count11 {
         }
     }
 
-    // Demo class with the main method
-    public class Count11Demo {
-
-        public static void main(String[] args) {
-
-            // Create an object of Count11 class
-            Count11 counter = new Count11();
-
-            // Test the count11 method with different strings
-            System.out.println("count11(\"11abc11\") → " + counter.count11("11abc11"));
-
-            System.out.println("count11(\"abc11x11x11\") → " + counter.count11("abc11x11x11"));
-
-            System.out.println("count11(\"111\") → " + counter.count11("111"));
-        }
-    }
-}
