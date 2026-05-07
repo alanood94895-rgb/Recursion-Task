38c87e62b7ac5e2b83b6ea438a9edad9b9085db3
CompanyProfile <alanood94895@gmail.com>
2026-05-07 11:14:34 +0400

    Triangle

--- src/Triangle.java
+++ src/Triangle.java
@@ -6,7 +6,6 @@ public class Triangle {
                 return 0;
             }
 
-            // Add the current row number and continue recursion
             return n + triangle(n - 1);
         }
     }
