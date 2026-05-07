b11fb738b856638f225a748355aa9841ee90cc30
CompanyProfile <alanood94895@gmail.com>
2026-05-07 09:51:54 +0400

    ChangePi

--- src/ChangePi.java
+++ src/ChangePi.java
@@ -6,8 +6,10 @@ public class ChangePi {
         }
         if (str.startsWith("pi")) {
             return "3.14" + changePi(str.substring(2));
-
-
-
         }
+        char firstChar = str.charAt(0);
+
+
+
+    }
 
