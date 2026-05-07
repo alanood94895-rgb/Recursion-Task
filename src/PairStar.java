ea1f9e5e30e2deb2f96f6d6a239c3aa04a5fafec
CompanyProfile <alanood94895@gmail.com>
2026-05-07 10:07:54 +0400

    PairStar

--- src/PairStar.java
+++ src/PairStar.java
@@ -2,7 +2,6 @@ public class PairStar {
     // Method to insert "*" between identical adjacent characters
     String pairStar(String str) {
 
-        // Base case: if string length is 0 or 1
         if (str.length() == 0 || str.length() == 1) {
             return str;
         }
