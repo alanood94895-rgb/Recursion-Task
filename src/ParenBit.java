1c10dd9285ff105fe976f41ab56b45d6f1f3e3ae
CompanyProfile <alanood94895@gmail.com>
2026-05-07 12:22:48 +0400

    ParenBit

--- src/ParenBit.java
+++ src/ParenBit.java
@@ -1,6 +1,4 @@
 public class ParenBit {
-    // Class that contains the recursive parenBit method
-    class ParenBit {
 
         // Method to extract substring inside parentheses (including them)
         String parenBit(String str) {
@@ -10,17 +8,14 @@ public class ParenBit {
                 return str;
             }
 
-            // If first character is not '(' remove it
             if (str.charAt(0) != '(') {
                 return parenBit(str.substring(1));
             }
 
-            // If last character is not ')' remove it
             if (str.charAt(str.length() - 1) != ')') {
                 return parenBit(str.substring(0, str.length() - 1));
             }
 
-            // Default return (safety, should not be reached often)
             return str;
         }
     }
