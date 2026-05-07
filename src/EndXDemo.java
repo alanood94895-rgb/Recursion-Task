b96af3829c660862cfccb6efdc56edf55c4f00c3
CompanyProfile <alanood94895@gmail.com>
2026-05-07 12:06:17 +0400

    EndXDemo

--- src/EndXDemo.java
+++ src/EndXDemo.java
@@ -1,12 +1,23 @@
 public class EndXDemo {
-    // Class that contains the recursive endX method
-    class EndX {
 
-        // Method to move all 'x' characters to the end of the string
-        String endX(String str) {
+            // Otherwise keep it in its original order
+            return firstChar + resultFromRest;
+        }
+    }
 
-            // Base case: if string is empty
-            if (str.length() == 0) {
-                return "";
-            }
+    // Demo class with the main method
+    public class EndXDemo {
 
+        public static void main(String[] args) {
+
+            // Create an object of EndX class
+            EndX mover = new EndX();
+
+            // Test the endX method with different strings
+            System.out.println("endX(\"xxre\") → \"" + mover.endX("xxre") + "\"");
+
+            System.out.println("endX(\"xxhixx\") → \"" + mover.endX("xxhixx") + "\"");
+
+            System.out.println("endX(\"xhixhix\") → \"" + mover.endX("xhixhix") + "\"");
+        }
+    }
