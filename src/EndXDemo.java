e1ecf606221049580a83f512a73f21b779a953d8
CompanyProfile <alanood94895@gmail.com>
2026-05-07 12:05:54 +0400

    EndXDemo

--- src/EndXDemo.java
+++ src/EndXDemo.java
@@ -10,19 +10,3 @@ public class EndXDemo {
                 return "";
             }
 
-    public class EndXDemo {
-
-        public static void main(String[] args) {
-
-            // Create an object of EndX class
-            EndX mover = new EndX();
-
-            // Test the endX method with different strings
-            System.out.println("endX(\"xxre\") → \"" + mover.endX("xxre") + "\"");
-
-            System.out.println("endX(\"xxhixx\") → \"" + mover.endX("xxhixx") + "\"");
-
-            System.out.println("endX(\"xhixhix\") → \"" + mover.endX("xhixhix") + "\"");
-        }
-    }
-}
