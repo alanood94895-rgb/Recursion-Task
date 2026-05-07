1e1f94dbbc2334342cecfd1278425a9d207b43c0
CompanyProfile <alanood94895@gmail.com>
2026-05-07 10:06:32 +0400

    PairStar

--- src/PairStar.java
+++ src/PairStar.java
@@ -30,7 +30,6 @@ public class PairStar {
         // Create an object of the PairStar class
         PairStar pair = new PairStar();
 
-        // Test the pairStar method with different strings
         System.out.println("pairStar(\"hello\") → \"" + pair.pairStar("hello") + "\"");
 
         System.out.println("pairStar(\"xxyy\") → \"" + pair.pairStar("xxyy") + "\"");
