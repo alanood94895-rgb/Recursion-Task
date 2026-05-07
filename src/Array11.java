586593311e0578b8bd2a4668ea196ba47ae67517
CompanyProfile <alanood94895@gmail.com>
2026-05-07 09:59:05 +0400

    Array11

--- src/Array11.java
+++ src/Array11.java
@@ -5,5 +5,9 @@ public class Array11 {
         if (nums[index] == 11){
             return 1 + array11(nums, index + 1);
         }
+
+        else{
+            return array11(nums, index + 1);
+        }
     }
 }
