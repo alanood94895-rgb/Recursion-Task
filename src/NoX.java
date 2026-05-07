468060b06d15e0b6a5799940ab1f11996cb49421
CompanyProfile <alanood94895@gmail.com>
2026-05-07 11:37:57 +0400

    NoX

--- src/NoX.java
+++ src/NoX.java
@@ -2,15 +2,12 @@ public class NoX {
 
         String noX(String str) {
 
-            // Base case: if string is empty
             if (str.length() == 0) {
                 return "";
             }
 
-            // Get the first character
             char firstChar = str.charAt(0);
 
-            // Process the remaining string
             String remaining = noX(str.substring(1));
 
             // If first character is 'x', skip it
