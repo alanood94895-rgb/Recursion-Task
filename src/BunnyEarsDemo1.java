c991ab1c601f7492ae9c754f0ec45162c2a2acb1
CompanyProfile <alanood94895@gmail.com>
2026-05-07 11:06:28 +0400

    BunnyEarsDemo1

--- src/BunnyEarsDemo1.java
+++ src/BunnyEarsDemo1.java
@@ -7,7 +7,7 @@ public class BunnyEarsDemo1 {
             // Test the bunnyEars method with different values
             System.out.println("bunnyEars(0) → " + bunny.BunnyEars(0));
 
-            System.out.println("bunnyEars(1) → " + bunny.BunnyEars(1));
+            System.out.println(bunny.bunnyEars2(1));
 
             System.out.println( bunny.bunnyEars2(2));
         }
