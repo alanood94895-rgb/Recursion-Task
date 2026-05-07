1f569819b74176b1278f1316f8479ddb4cc2cfa4
CompanyProfile <alanood94895@gmail.com>
2026-05-07 12:46:58 +0400

    StrCopies

--- src/StrCopies.java
+++ src/StrCopies.java
@@ -1,11 +1,8 @@
 public class StrCopies {
-    // Class that contains the recursive strCopies method
-    class StrCopies {
 
         // Method to check if sub appears at least n times (overlapping allowed)
         boolean strCopies(String str, String sub, int n) {
 
-            // Base case: if required count is satisfied
             if (n == 0) {
                 return true;
             }
