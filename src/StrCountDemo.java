bae5dde08c3e416a6ea93f5486fa8eea239a9412
CompanyProfile <alanood94895@gmail.com>
2026-05-07 10:48:19 +0400

    StrCount

--- src/StrCountDemo.java
+++ src/StrCountDemo.java
@@ -3,12 +3,11 @@ public class StrCountDemo {
         public static void main(String[] args) {
             StrCount counter = new StrCount();
 
-            System.out.println("strCount(\"catcowcat\", \"cat\") → "
-                    + counter.strCount("catcowcat", "cat"));
+            System.out.println(counter.strCount("catcowcat", "cat"));
 
             System.out.println(counter.strCount("catcowcat", "cow"));
 
             System.out.println(counter.strCount("catcowcat", "dog"));
         }
     }
-}
+
