9b7c90f24f32c841f3e52a709cd4f3c9a6d9ea04
CompanyProfile <alanood94895@gmail.com>
2026-05-07 12:16:33 +0400

    Count11Demo

--- src/Count11Demo.java
+++ src/Count11Demo.java
@@ -5,7 +5,6 @@ public class Count11Demo {
             // Create an object of Count11 class
             Count11 counter = new Count11();
 
-            // Test the count11 method with different strings
             System.out.println(counter.count11("11abc11"));
 
             System.out.println( counter.count11("abc11x11x11"));
