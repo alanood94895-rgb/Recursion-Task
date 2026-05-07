e4433aca77f25fa7a8a11560baa5ea45f3e7cd19
CompanyProfile <alanood94895@gmail.com>
2026-05-07 10:14:56 +0400

    CountAbc

--- src/CountAbc.java
+++ src/CountAbc.java
@@ -29,6 +29,5 @@ public class CountAbc {
         public static void main(String[] args) {
 
             // Create an object of the CountAbc class
-            CountAbc counter = new CountAbc();
 
 
