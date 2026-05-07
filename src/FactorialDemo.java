6997480aacfb4653b1853f1e9ac1c33c86ad7265
CompanyProfile <alanood94895@gmail.com>
2026-05-07 08:51:11 +0400

    FactorialDemo calss

--- src/FactorialDemo.java
+++ src/FactorialDemo.java
@@ -2,6 +2,7 @@ public class FactorialDemo {
     public static void main(String[] args) {
 
         System.out.println();
+        System.out.println();
 
         Factorial fact = new Factorial();
 
