c2965e100a044507a2743fe72cf28f3688d9a866
CompanyProfile <alanood94895@gmail.com>
2026-05-07 13:29:07 +0400

    SumDigitsDemo

---
+++ src/SumDigitsDemo.java
@@ -0,0 +1,13 @@
+public class SumDigitsDemo {
+        public static void main(String[] args) {
+
+            SumDigits sum = new SumDigits();
+
+            System.out.println( sum.sumDigits(126));
+
+            System.out.println( sum.sumDigits(49));
+
+            System.out.println( sum.sumDigits(12));
+        }
+    }
+
