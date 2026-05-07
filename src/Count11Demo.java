20929efd80622282e3353da7244c49019f707c3b
CompanyProfile <alanood94895@gmail.com>
2026-05-07 12:15:47 +0400

    Count11Demo

--- src/Count11Demo.java
+++ src/Count11Demo.java
@@ -1,13 +1,6 @@
 public class Count11Demo {
 
-    // Class that contains the recursive count11 method
-    class Count11 {
 
-        // Method to count non-overlapping occurrences of "11"
-        int count11(String str) {
-
-
-    // Demo class with the main method
     public class Count11Demo {
 
         public static void main(String[] args) {
