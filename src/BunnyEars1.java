7218aeff5f5ad8f04b63f60576c34784e4b4cba5
CompanyProfile <alanood94895@gmail.com>
2026-05-07 10:54:30 +0400

    BunnyEars1

--- src/BunnyEars1.java
+++ src/BunnyEars1.java
@@ -1,7 +1,5 @@
 public class BunnyEars1 {
-    // Class that contains the recursive bunnyEars method
 
-        // Method to calculate total bunny ears
         int bunnyEars(int n) {
 
             if (n == 0) {
