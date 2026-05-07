e8455cd16e16fb1a1f7d9c057895c8ca07a5e357
CompanyProfile <alanood94895@gmail.com>
2026-05-07 10:15:43 +0400

    CountAbc

--- src/CountAbc.java
+++ src/CountAbc.java
@@ -14,7 +14,6 @@ public class CountAbc {
             // Check if the substring is "abc" or "aba"
             if (firstThree.equals("abc") || firstThree.equals("aba")) {
 
-                // Count the match and continue recursion
                 return 1 + countAbc(str.substring(1));
             }
 
