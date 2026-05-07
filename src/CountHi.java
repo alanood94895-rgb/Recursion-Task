79a2d3d3c4e428221d8448539ac888f045ca5b5e
CompanyProfile <alanood94895@gmail.com>
2026-05-07 11:32:04 +0400

    CountHi

--- src/CountHi.java
+++ src/CountHi.java
@@ -11,7 +11,6 @@ class CountHi {
             return 0;
         }
 
-        // Check if the first two characters are "hi"
         if (str.substring(0, 2).equals("hi")) {
 
             return 1 + countHi(str.substring(2));
