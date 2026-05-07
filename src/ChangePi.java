4647594d48f6a10a0b791970f41198404b3563dc
CompanyProfile <alanood94895@gmail.com>
2026-05-07 09:52:12 +0400

    ChangePi

--- src/ChangePi.java
+++ src/ChangePi.java
@@ -8,8 +8,8 @@ public class ChangePi {
             return "3.14" + changePi(str.substring(2));
         }
         char firstChar = str.charAt(0);
-
-
-
+        return firstChar + changePi(str.substring(1));
     }
+}
+
 
