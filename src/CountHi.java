ea025e6f6a88a67dcf276be8bd71660dd5d5816a
CompanyProfile <alanood94895@gmail.com>
2026-05-07 11:30:41 +0400

    CountHi

--- src/CountHi.java
+++ src/CountHi.java
@@ -27,15 +27,3 @@ class CountHi {
 public class CountHiDemo {
 
     public static void main(String[] args) {
-
-        // Create object of CountHi class
-        CountHi counter = new CountHi();
-
-        // Test the countHi method with different strings
-        System.out.println("countHi(\"xxhixx\") → " + counter.countHi("xxhixx"));
-
-        System.out.println("countHi(\"xhixhix\") → " + counter.countHi("xhixhix"));
-
-        System.out.println("countHi(\"hi\") → " + counter.countHi("hi"));
-    }
-}
