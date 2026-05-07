de9517bd45496c8a901ff62facb04aee73840d1d
CompanyProfile <alanood94895@gmail.com>
2026-05-07 11:36:08 +0400

    CountHiDemo

--- src/CountHiDemo.java
+++ src/CountHiDemo.java
@@ -6,7 +6,7 @@ public class CountHiDemo {
 
             System.out.println( counter.countHi("xxhixx"));
 
-            System.out.println("countHi(\"xhixhix\") → " + counter.countHi("xhixhix"));
+            System.out.println(counter.countHi("xhixhix"));
 
             System.out.println("countHi(\"hi\") → " + counter.countHi("hi"));
         }
