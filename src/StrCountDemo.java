c886352f885eee5f7f58a09a5bd86bd21bcc5ff8
CompanyProfile <alanood94895@gmail.com>
2026-05-07 10:29:38 +0400

    StrCount

--- src/StrCountDemo.java
+++ src/StrCountDemo.java
@@ -3,7 +3,6 @@ public class StrCountDemo {
         public static void main(String[] args) {
             StrCount counter = new StrCount();
 
-            // Test the strCount method with different substrings
             System.out.println("strCount(\"catcowcat\", \"cat\") → "
                     + counter.strCount("catcowcat", "cat"));
 
