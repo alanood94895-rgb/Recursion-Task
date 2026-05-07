ba53479ba9fe98a018492661788e0c9ed1b36a51
CompanyProfile <alanood94895@gmail.com>
2026-05-07 09:01:27 +0400

    create method for BunnyEars class

--- src/BunnyEars.java
+++ src/BunnyEars.java
@@ -1,16 +1,15 @@
 public class BunnyEars {
 
-    int bunnyEars2 (int n) {
+    int bunnyEars2(int n) {
 
-        if (n== 0){
+        if (n == 0) {
             return 0;
         }
-        if(n % 2 == 0){
-            return  3 + bunnyEars2(n - 1);
-        }
-      else {
+        if (n % 2 == 0) {
+            return 3 + bunnyEars2(n - 1);
+        } else {
             return 2 + bunnyEars2(n - 1);
         }
     }
+}
 
-    }
