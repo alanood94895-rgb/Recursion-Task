699455f97cfab03cfd3285223f260da5e9165d78
CompanyProfile <alanood94895@gmail.com>
2026-05-07 12:56:50 +0400

    StrCopiesDemo

--- src/StrCopiesDemo.java
+++ src/StrCopiesDemo.java
@@ -8,8 +8,7 @@ public class StrCopiesDemo {
             System.out.println("strCopies(\"catcowcat\", \"cat\", 2) → "
                     + checker.strCopies("catcowcat", "cat", 2));
 
-            System.out.println("strCopies(\"catcowcat\", \"cow\", 2) → "
-                    + checker.strCopies("catcowcat", "cow", 2));
+            System.out.println( checker.strCopies("catcowcat", "cow", 2));
 
             System.out.println(checker.strCopies("catcowcat", "cow", 1));
         }
