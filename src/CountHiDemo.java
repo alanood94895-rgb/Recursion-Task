2728bc1fe4cd657871f36a9bfbb2172132dce4f5
CompanyProfile <alanood94895@gmail.com>
2026-05-07 11:33:42 +0400

    CountHiDemo

--- src/CountHiDemo.java
+++ src/CountHiDemo.java
@@ -2,19 +2,7 @@ public class CountHiDemo {
     // Class that contains the recursive countHi method
     class CountHi {
 
-        // Method to count occurrences of "hi" using recursion
-        int countHi(String str) {
 
-            // Base case: if string has less than 2 characters
-            if (str.length() < 2) {
-                return 0;
-            }
-
-            // Check if the first two characters are "hi"
-            if (str.substring(0, 2).equals("hi")) {
-
-                // Count this occurrence and skip 2 characters
-                return 1 + countHi(str.substring(2));
 
 
         public static void main(String[] args) {
