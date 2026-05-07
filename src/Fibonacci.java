af8a301f7c14d276b1b371afd54dae50840226eb
CompanyProfile <alanood94895@gmail.com>
2026-05-07 13:21:04 +0400

    Fibonacci

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,2 +1,25 @@
 public class Fibonacci {
+    // Class that contains the recursive fibonacci method
+    class Fibonacci {
+
+        // Method to calculate Fibonacci numbers using recursion
+        int fibonacci(int n) {
+
+            // Base case: fibonacci(0) = 0
+            if (n == 0) {
+                return 0;
+            }
+
+            // Base case: fibonacci(1) = 1
+            if (n == 1) {
+                return 1;
+            }
+
+            // Recursive case
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+
+
+
 }
