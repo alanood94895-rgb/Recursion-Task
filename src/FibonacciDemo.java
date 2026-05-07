7a348e3d23c40f200a7d9fb94ef75f0d9cdb6482
CompanyProfile <alanood94895@gmail.com>
2026-05-07 13:25:14 +0400

    FibonacciDemo

--- src/FibonacciDemo.java
+++ src/FibonacciDemo.java
@@ -1,12 +1,9 @@
 public class FibonacciDemo {
 
-
         public static void main(String[] args) {
 
-            // Create an object of Fibonacci class
             Fibonacci fib = new Fibonacci();
 
-            // Test the fibonacci method with different values
             System.out.println("fibonacci(0) → " + fib.fibonacci(0));
 
             System.out.println("fibonacci(1) → " + fib.fibonacci(1));
@@ -18,4 +15,4 @@ public class FibonacciDemo {
             System.out.println("fibonacci(10) → " + fib.fibonacci(10));
         }
     }
-}
+
